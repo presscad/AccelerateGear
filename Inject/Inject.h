aa025d9dc8b925025b938f3face195042ecb035f
hyhlfq <hyhlfq@gmail.com>
2014-04-17 11:24:19 +0800

    创建项目，Inject和dll Inject采用MFC

---
+++ Inject/Inject.h
@@ -0,0 +1,31 @@
+// Inject.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CInjectApp:
+// 有关此类的实现，请参阅 Inject.cpp
+//
+
+class CInjectApp : public CWinApp
+{
+public:
+	CInjectApp();
+
+// 重写
+	public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CInjectApp theApp;
